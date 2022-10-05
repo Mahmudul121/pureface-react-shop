1956aeebab2525d41557c9e229ac7e84cac1d6f0
mahmudul121 <shohagm.iotait.com>
2022-10-06 01:57:57 +0600

    issue fixed 06-10-2022

--- src/components/common/default-seo.tsx
+++ src/components/common/default-seo.tsx
@@ -1,4 +1,4 @@
-import { DefaultSeo as NextDefaultSeo } from 'next-seo';
+import { DefaultSeo as NextDefaultSeo } from "next-seo";
 import { useSettings } from "@contexts/settings.context";
 
 const DefaultSeo = () => {
@@ -7,37 +7,37 @@ const DefaultSeo = () => {
     <NextDefaultSeo
       additionalMetaTags={[
         {
-          name: 'viewport',
-          content: 'width=device-width, initial-scale=1 maximum-scale=1',
+          name: "viewport",
+          content: "width=device-width, initial-scale=1 maximum-scale=1",
         },
         {
-          name: 'apple-mobile-web-app-capable',
-          content: 'yes',
+          name: "apple-mobile-web-app-capable",
+          content: "yes",
         },
         {
-          name: 'theme-color',
-          content: '#ffffff',
+          name: "theme-color",
+          content: "#ffffff",
         },
       ]}
       additionalLinkTags={[
         {
-          rel: 'apple-touch-icon',
-          href: 'icons/apple-icon-180.png',
+          rel: "apple-touch-icon",
+          href: "icons/apple-icon-180.png",
         },
         {
-          rel: 'manifest',
-          href: '/manifest.json',
+          rel: "manifest",
+          href: "/manifest.json",
         },
       ]}
-      title={settings?.seo?.metaTitle || settings?.siteTitle || 'E-Commerce'}
-      defaultTitle="ChawkBazar"
+      title={settings?.seo?.metaTitle || settings?.siteTitle || "E-Commerce"}
+      defaultTitle="Pureface"
       description={settings?.seo?.metaDescription || settings?.siteSubtitle}
       canonical={settings?.seo?.canonicalUrl}
       openGraph={{
         title: settings?.seo?.ogTitle,
         description: settings?.seo?.ogDescription,
-        type: 'website',
-        locale: 'en_US',
+        type: "website",
+        locale: "en_US",
         site_name: settings?.siteTitle,
         images: [
           {
